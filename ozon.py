792c46b0199cab2bd5c15773fa7eef7c915ab3f4
rafa <91733345+jakezetci@users.noreply.github.com>
2022-02-10 04:10:01 +0300

    version 3.0: better parser, better graph, font fix

--- ozon.py
+++ ozon.py
@@ -4,23 +4,61 @@ from scipy.io import netcdf
 import numpy as np
 import json
 import matplotlib.pyplot as plt
+import matplotlib
 import argparse
 from geopy.geocoders import Nominatim
 
 
-"""
-for getting proper labels for countries with non-Latin-Cyrillic-Arabic alphabet
-you might need a non-default matplotlib font
-for easier tests i didn't ask it to import any - you might get runtime errors
-"""
+def is_float(element):
+    try:
+        float(element)
+        return True
+    except ValueError:
+        return False
+
+
+def location_font(location):
+    """
+    finds the appropriate font for your location
+    :param location: your location
+    :type location: location.Location
+    :return: fontstyle
+    :rtype: matplotlib.font_manager.FontProperties
+
+    """
+    style = matplotlib.font_manager.FontProperties()
+    style.set_size('xx-large')
+    style.set_family(['Calibri', 'Helvetica', 'Arial', 'serif'])
+    addr_dict = location.raw["address"]
+    keys = ['jp', 'cn', 'kr', 'kp', 'mm', 'mn', 'th']
+    countries_dict = {
+        'cn': "SimSun",
+        'th': "Leelawadee UI",
+        'kr': 'Malgun Gothic',
+        'jp': 'MS Gothic',
+        'kp': 'Malgun Gothic',
+        'mm': "Myanmar Text",
+        'mn': "Mongolian Baiti"
+                     }  # не панацея, но кажется, это все языки вне Calibri
+    code = addr_dict["country_code"]
+    if code in keys:
+        style.set_family([countries_dict[code],
+                         'Calibri', 'Helvetica', 'Arial', 'serif'])
+    return style
+#  вообще проблему языков и шрифтов должен решать familyfont, а не моя функция
+#  но матплотлиб его использует лишь для проверки существования шрифта в целом,
+#  а не каждого глифа
+
+#  а, и ещё надо бы создать для каждого языка целую семью шрифтов, но мне лень
+#  если у вас не нашлось этого шрифта на компе - простите уж, могу поправить
 
 
 def location_city(location):
     """
     shortens the location.address to just city/village + country
-    :param location: your location with location.raw["address]
+    :param location: your location with location.raw["address"]
     :type location: location.Location
-    :return: short format of an address or None is there is no address
+    :return: short format of an address or None if there is no address
     :rtype: str
 
     """
@@ -29,26 +67,21 @@ def location_city(location):
     for key in village_KEYS:
         if key in addr_dict:
             return ("".join([addr_dict["country"], ", ", addr_dict[key]]))
-    if 'locality' in addr_dict:
+    if 'country' in addr_dict:
+        return(addr_dict['country'])
+    elif 'locality' in addr_dict:
         return (addr_dict['locality'])
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument('pos', metavar='POS', type=str,
-                    help='Position, location name or "longitude  latitude"',
+                    help='Position: location name or "longitude  latitude"',
                     default=None, nargs='*')
 args = parser.parse_args()
-if len(args.pos) == 1:
-    loc = args.pos
-    geolocator = Nominatim(user_agent="Ozone Stuff")
-    location = geolocator.geocode(loc)
-    latitude = location.latitude
-    longitude = location.longitude
-    a = " ".join(str(e) for e in [latitude, longitude])
-    # такой некрасивый переворот происходит из-за неоднозначности geopy,
-    # иначе он не даст нужного location.raw
-    location = geolocator.reverse(a)
-elif len(args.pos) == 2:
+
+if is_float(args.pos[0]):
+    if len(args.pos) != 2:
+        raise NotImplementedError("wrong format")
     longitude = float(args.pos[0])
     latitude = float(args.pos[1])
     geolocator = Nominatim(user_agent="Ozone Stuff")
@@ -56,7 +89,20 @@ elif len(args.pos) == 2:
     # приходится реверсить потому что в задании просят сначала подавать долготу
     location = geolocator.reverse(a)
 else:
-    print("wrong format")
+    if len(args.pos) == 1:
+        loc = args.pos
+    else:
+        loc = " ".join(str(e) for e in args.pos)
+    geolocator = Nominatim(user_agent="Ozone Stuff")
+    location = geolocator.geocode(loc)
+    if location is None:
+        raise NotImplementedError("wrong format")
+    latitude = location.latitude
+    longitude = location.longitude
+    a = " ".join(str(e) for e in [latitude, longitude])
+    # такой некрасивый переворот происходит из-за неоднозначности geopy,
+    # иначе он не даст нужного location.raw
+    location = geolocator.reverse(a)
 
 
 if __name__ == "__main__":
@@ -96,9 +142,9 @@ if __name__ == "__main__":
                 maxi[1] = data
             if data < mini[1]:
                 mini[1] = data
-    mean[2] = float(mean[2])/n_of_months
-    mean[1] = float(mean[1])*12/n_of_months
-    mean[0] = float(mean[0])*12/n_of_months
+    mean[2] = np.round(float(mean[2])/n_of_months, decimals=2)
+    mean[1] = np.round(float(mean[1])*12/n_of_months, decimals=2)
+    mean[0] = np.round(float(mean[0])*12/n_of_months, decimals=2)
     for k in range(len(names)):
         d[names[k]] = {
             'min': float(mini[k]),
@@ -108,28 +154,30 @@ if __name__ == "__main__":
         json.dump(d, f)
 
     """построение графиков"""
-    fig = plt.figure()
+    fig = plt.figure(figsize=(20, 9))
     ax = fig.add_subplot(1, 1, 1)
     ax.spines['bottom'].set_position(['axes', 0.0])
     ax.spines['right'].set_color('none')
     ax.spines['top'].set_color('none')
     ax.xaxis.set_ticks_position('bottom')
     ax.yaxis.set_ticks_position('left')
-    ax.set_xlabel('years')
-    ax.set_ylabel('ozon column')
+    ax.set_xlabel('years', fontfamily="Calibri", fontsize='xx-large')
+    ax.set_ylabel('ozon column, Dobson units', fontfamily='Calibri',
+                  fontsize='xx-large')
     january = np.transpose(dots[0])
     july = np.transpose(dots[1])
     overall = np.transpose(dots[2])
     jan_plot, = plt.plot(january[1], january[0], 'b', ls='--')
-    jul_plot, = plt.plot(july[1], july[0], 'g', ls='--')
-    all_plot, = plt.plot(overall[1], overall[0], 'r', lw=0.4)
-    jan_plot.set_label('january')
-    jul_plot.set_label('july')
-    all_plot.set_label('all data')
+    jul_plot, = plt.plot(july[1], july[0], 'red', ls='--')
+    all_plot, = plt.plot(overall[1], overall[0], 'grey', lw=1)
+    plt.fill_between(overall[1], overall[0], color='grey', alpha=0.4)
+    jan_plot.set_label('ozone level during january')
+    jul_plot.set_label('ozone level during july')
+    all_plot.set_label('ozone level throughout the years')
     if location is not None:  # предотвращает случаи поиска селения в океане
         d, = plt.plot([], [], 'o')
-        c = location.address
-        b = location
         d.set_label(location_city(location))
-    ax.legend()
+        style = location_font(location)
+    ax.set_ylim([mini[2]-5.0, maxi[2]+5.0])
+    ax.legend(loc='best', prop=style)
     plt.savefig('ozon.png')
